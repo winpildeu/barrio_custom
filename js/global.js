56ebe5107f6c93ea381669b99f6cd0f17edf03cb
Winfield H <winterfreshness@gmail.com>
2019-03-28 22:32:24 -0400

    initial commit

---
+++ js/global.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+  "use strict";
+
+  Drupal.behaviors.barrio_custom = {
+    attach: function(context, settings) {}
+  };
+})(jQuery, Drupal);
